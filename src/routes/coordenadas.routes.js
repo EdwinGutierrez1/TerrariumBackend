9484e9edeee5f2534ee4a6e83399b01fbdbe967f
Edwin Gutierrez <egutierrez11@udi.edu.co>
2025-05-13 18:51:41 -0500

    primeros pasos en la visualizacion de arboles en el mapa

--- src/routes/coordenadas.routes.js
+++ src/routes/coordenadas.routes.js
@@ -1,17 +1,25 @@
-const express = require('express'); //para definir rutas
+const express = require("express"); //para definir rutas
 const router = express.Router(); // Creamos una instancia del router
 
 // Importamos el controlador que maneja la lógica de coordenadas
-const coordenadasController = require('../controllers/coordenadas.controller');
+const coordenadasController = require("../controllers/coordenadas.controller");
 
 // Middleware de autenticación para proteger las rutas
-const verifyToken = require('../middlewares/auth.middleware');
+const verifyToken = require("../middlewares/auth.middleware");
 
 // Ruta para obtener las coordenadas de las subparcelas
-router.get('/subparcelas', verifyToken, coordenadasController.getCoordenadasSubparcelas);
+router.get(
+  "/subparcelas",
+  verifyToken,
+  coordenadasController.getCoordenadasSubparcelas
+);
 
 // Ruta para obtener el centro poblado de la brigada
-router.get('/centro-poblado', verifyToken, coordenadasController.getCentroPoblado);
+router.get(
+  "/centro-poblado",
+  verifyToken,
+  coordenadasController.getCentroPoblado
+);
 
 //  Exportamos el router para ser usado en la configuración principal de rutas (app.js)
 module.exports = router;
